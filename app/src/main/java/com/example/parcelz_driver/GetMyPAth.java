dd71c1533e051b60489d8e3efbbcdc92c6f8039e
Ali Zidelkhir <a.zidelkhir.inf@lagh-univ.dz>
2021-06-09 01:46:31 +0100

    8 juin 2021

--- app/src/main/java/com/example/parcelz_driver/GetMyPAth.java
+++ app/src/main/java/com/example/parcelz_driver/GetMyPAth.java
@@ -4,10 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.location.Geocoder;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.widget.RadioButton;
-import android.widget.Switch;
-import android.widget.TextView;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.maps.GoogleMap;
@@ -19,27 +17,27 @@ import com.shuhart.stepview.StepView;
 
 import java.util.List;
 
-public class AcceptRequest extends AppCompatActivity {
-    String Key = "";
+public class GetMyPAth extends AppCompatActivity {
+
+
     private GoogleMap map;
     private CameraPosition cameraPosition;
-    DatabaseReference databaseReference;
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
-    Geocoder geocoder;
-    Switch switchFrangible;
+    LocationManager locationManager;
+
     // The entry point to the Fused Location Provider.
     private FusedLocationProviderClient fusedLocationProviderClient;
 
     // A default location (Sydney, Australia) and default zoom to use when location permission is
     // not granted.
     private final LatLng defaultLocation = new LatLng(-33.8523341, 151.2106085);
-    private static final int DEFAULT_ZOOM = 15;
+    private static final int DEFAULT_ZOOM = 10;
     private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
     private boolean locationPermissionGranted;
 
     // The geographical location where the device is currently located. That is, the last-known
     // location retrieved by the Fused Location Provider.
     private Location lastKnownLocation;
+
     // Keys for storing activity state.
     private static final String KEY_CAMERA_POSITION = "camera_position";
     private static final String KEY_LOCATION = "location";
@@ -51,13 +49,20 @@ public class AcceptRequest extends AppCompatActivity {
     private List[] likelyPlaceAttributions;
     private LatLng[] likelyPlaceLatLngs;
     StepView stepView;
-    double LATT, LONGG, LATTDEST, LONGGDEST;
-    RadioButton PriceBase, TimeBase, radioDelivery, radioPickup;
-    TextView days, pricing;
+    int stepIndex = 0;
+    //DB Real Time Database
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference databaseReference;
+    String Key = "";
+    double Lat, Lang;
+    Geocoder geocoder;
+
+
+    double LATT, LONGG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_accept_request);
+        setContentView(R.layout.activity_get_my_path);
     }
 }
